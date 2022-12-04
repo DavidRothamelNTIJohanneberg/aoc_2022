cff534d02a13a9f50f49e63092d3107d2e32b432
DavidRothamelNTIJohanneberg <rothamel.david@gmail.com>
2022-12-04 21:14:11 +0100

    HElp

--- day4/day4.py
+++ day4/day4.py
@@ -1,15 +1,9 @@
 from filereader import filerdl
 
 def comparer(pair1, pair2):
-    #Gör om helt och hållet
-    print(pair1)
-    print(pair2)
-    if pair1 == pair2:
-        return False
-    if int(str1) <= int(str2):
-        if int(str1) <= int(str2):
-            return True
-    elif int(pair1[1]) <= int(pair2[1]):
+    if pair1 in pair2:
+        return True
+    if pair2 in pair1:
         return True
         
 def num_range(lista):
@@ -22,16 +16,18 @@ def contains(text):
     counter = 0
     lista = []
     temp = []
+    #Delar upp varje rad till två ranges
     for i in text:
        temp.append(i.split(","))
-       
+    #Delar upp varje objekt i listan till två strings
     for x in temp:
         for y in x:
             lista.append(y.split("-"))
     
-    print(lista)
-    
+    print(len(lista))
+    #Kollar om det går att hitta hela den ena rangen i den andra
     for i in range(0, len(lista), 2):
+        print(i)
         if comparer(num_range(lista[i]), num_range(lista[i + 1])):
             counter += 1
         
